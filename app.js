874cd61236c34fa0ad461713ab8ba491917d9ba7
GenjitsuGame <pascal.luttgens@hotmail.fr>
2016-02-21 21:16:28 +0100

    Fixed package.json for release version

--- app.js
+++ app.js
@@ -29,18 +29,6 @@ const bot = new DiscordClient({
 bot.on('ready', function () {
     winston.info('Bot started');
 
-    setTimeout(() => {
-        bot.sendMessage({
-            to: '141179913192341504',
-            message: 'Check me out on github : https://github.com/GenjitsuGame/bot-mmo-client'
-        });
-
-        //bot.sendMessage({
-        //    to: '150019318262792192',
-        //    message: 'Check me out on github : https://github.com/GenjitsuGame/bot-mmo-client'
-        //});
-    }, 1000 * 60 * 30);
-
     require('./helpers/messageBuffer').construct(bot);
 
     // main
